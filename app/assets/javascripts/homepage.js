3a632791029f4c568fa35856b9357a2919792e9d
tboneyang <tboneyang@gmail.com>
2015-01-18 23:32:11 -0500

    updates to homepage and color scheme

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,8 +1,74 @@
+var showschedule = function(){
+  $("#feab").removeClass("selected");
+  $("#regb").removeClass("selected");
+  $("#schb").addClass("selected");
 
+  $("#homenavbox").empty();
+  var topbar = $("<div></div>").addClass("htopbar");
+  var tsearch = $("<input></input>").addClass("htopsearch").attr("placeholder", "Search Summoners");
+  topbar.append(tsearch);
+  $("#homenavbox").append(topbar);
+}
+
+var showregister =function(){
+  $("#feab").removeClass("selected");
+  $("#regb").addClass("selected");
+  $("#schb").removeClass("selected");
+
+  $("#homenavbox").empty();
+  var topbar = $("<div></div>").addClass("htopbar");
+  var tsearch = $("<input></input>").addClass("htopsearch").attr("placeholder", "Search Summoners");
+  topbar.append(tsearch);
+  $("#homenavbox").append(topbar);
+}
+
+var showfeature = function(){
+  $("#feab").addClass("selected");
+  $("#regb").removeClass("selected");
+  $("#schb").removeClass("selected");
+
+  $("#homenavbox").empty();
+  var topbar = $("<div></div>").addClass("htopbar");
+  var tsearch = $("<input></input>").addClass("htopsearch").attr("placeholder", "Search Summoners");
+  topbar.append(tsearch);
+  $("#homenavbox").append(topbar);
+
+  var table = $('<table></table>').addClass('homestandings');
+  var firstrow = $('<tr></tr>').addClass('toprow');;
+
+  firstrow.append('<td>Rank</td>');
+  firstrow.append('<td>Summoner Name</td>');
+  firstrow.append('<td>Pledges</td>');
+  table.append(firstrow);
+
+  var sumdata = ["lol","faker","wildturtle", "madglory prime", "scarra", "doctor yang", "unblevable", "omniscience", "the odd one", "trick2g", "annie bot", "dyrus", "nightblue3"];
+
+  for(i=1;i<11;i++){
+    var row = $('<tr></tr>');
+    var rank =$('<td>Rank</td>').text(i).addClass("frowd");
+    row.append(rank);
+    var sum=$('<td></td>').text(sumdata[i]).addClass("frowd");
+    row.append(sum);
+
+    var r = (11-i)*10;
+    var pledge=$('<td>Pledges</td>').text("$"+r).addClass("frowd");
+
+    var gdata = ["fff","fee","fdd","fcc","fbb","faa","f99","f88","f77","f66","f55", "f44"]
+    var gradient = "#"+gdata[11-i];
+    row.css("background-color", gradient);
+
+    row.append(pledge);
+    table.append(row);
+  }
+
+  $("#homenavbox").append(table);
+
+}
 
 $(document).ready(function(){
 
 	//document.setTimeout(function(){}, 1000);
+  showfeature();
 
 	var donation = 19765;
 	  $({someValue: 19000}).animate({someValue: donation}, {
